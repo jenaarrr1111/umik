3b2674d2b02c3d0308142edb7e59ab06d335f96a
markus-langgeng <markus.langgeng4@gmail.com>
2023-01-22 05:37:16 +0700

    menambahkan partial search bar

--- resources/views/main-content/umkm/index.blade.php
+++ resources/views/main-content/umkm/index.blade.php
@@ -3,5 +3,7 @@
         <h1 class="text-3xl font-bold my-5">{{ $title }}</h1>
     </header>
 
+    @include('partials.search')
+
     <p>Halo, ini adalah halaman {{ $title }}</p>
 </x-layout>
--- resources/views/main-content/users/index.blade.php
+++ resources/views/main-content/users/index.blade.php
@@ -3,6 +3,7 @@
         <h1 class="text-3xl font-bold my-5">{{ $title }}</h1>
     </header>
 
+    @include('partials.search')
 
     @if (count($users) == 0)
     <div class="p-5 shadow-xl min-h-[50%] overflow-auto">
@@ -10,7 +11,7 @@
     </div>
     @else
 
-    <div class="p-5 shadow-xl min-h-[50%] overflow-auto mb-5">
+    <div class="p-5 shadow-lg bg-slate-50 min-h-[50%] overflow-auto mb-5">
         <x-tables.user :data="$users" />
     </div>
     @endif
